6959b9984a9f05facd2b982350791afcef6b1813
karen9999 <karen.dev999@gmail.com>
2020-04-23 00:21:08 +0300

    fix

--- src/commander.js
+++ src/commander.js
@@ -8,7 +8,7 @@ program
   .arguments("<firstConfig> <secondConfig>")
   .description("Compares two configuration files and shows a difference.")
   .option("-f, --format [type]", "output format")
-  .action(function (firstConfig, secondConfig) {
+  .action(function getPath (firstConfig, secondConfig) {
     const file1 = JSON.parse(fs.readFileSync(firstConfig));
     const file2 = JSON.parse(fs.readFileSync(secondConfig));
     console.log(genDifference(file1, file2));
